d9cf7254ef65c5f10c4cedeec6ae72068b5de1b0
Alexandra Amihaiesi <alexandraamihaiesi@alexandra.local>
2020-08-19 08:53:10 +0300

    Updates

---
+++ precache-manifest.44202ff05e2bf0cd0dacb6c8e11ac5ad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c137fe4d0b4fc300f24f8f08e952902d",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "194f60b19bf00fa93e24",
+    "url": "/monsters/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "225061d95169635c7526",
+    "url": "/monsters/static/js/2.030e2e47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.030e2e47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "194f60b19bf00fa93e24",
+    "url": "/monsters/static/js/main.e7927c62.chunk.js"
+  },
+  {
+    "revision": "87b4cc9a4f78073ee66f",
+    "url": "/monsters/static/js/runtime-main.fb694cd0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters/static/media/logo.5d5d9eef.svg"
+  }
+]);
